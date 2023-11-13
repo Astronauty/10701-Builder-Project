71d1efd2be6808c905eaf952b701c3cab1bcd9d4
jeremy <jezzabeard@gmail.com>
2023-11-13 12:43:39 -0500

    Made data_loader.py into just functions to avoid repickling data every time. Also added ability to make the data_points lists of tokens rather than strings. Also initial commit of rnn. Mostly seems to work, but still not managed to get the BLEU metric to work.

--- data_loader.py
+++ data_loader.py
@@ -6,12 +6,7 @@ import pickle
 from tqdm import tqdm
 import string
 
-# tokenizers
-en_tokenizer = get_tokenizer(tokenizer='spacy',language='en_core_web_sm')
-fr_tokenizer = get_tokenizer(tokenizer='spacy',language='fr_core_news_sm')
 
-# get the whole data from csv
-whole_data = pd.read_csv("archive/en-fr.csv",encoding="utf-8", keep_default_na=False)
 
 # Langs class
 class Langs:
@@ -29,7 +24,7 @@ class Langs:
             self.index2word[len(self.index2word)] = i
         # the total number of special words
         self.n_words = len(self.index2word)
-    def addSentence(self, sentence):
+    def addSentence(self, sentence, en_tokenizer, fr_tokenizer):
         if self.lang == "en":
             tokens = en_tokenizer(sentence)
         else:
@@ -53,14 +48,32 @@ def read_lang(eng_str="en", fr_str="fr", data_pd=None):
         print("wrong data format!!")
         return
     # get the pairs
-    pairs = [(data_pd[eng_str][i], data_pd[fr_str][i]) for i in tqdm(range(len(whole_data)))]
+    pairs = [(data_pd[eng_str][i], data_pd[fr_str][i]) for i in tqdm(range(len(data_pd)))]
     
     # create the class objects of Langs for English and French to count the 
     eng_lang = Langs("en")
     fr_lang = Langs("fr")
     return eng_lang, fr_lang, pairs
 
-def data_preprocessing(eng_str="en", fr_str="fr", data_pd=None):
+def string_to_token_list(string, lang, en_tokenizer, fr_tokenizer):
+    token_list = []
+    if lang.lang == "en":
+        words = en_tokenizer(string.lower())
+    else:
+        words = fr_tokenizer(string.lower())
+    for word in words:
+        token_list.append(lang.word2index[word])
+    return token_list
+
+def string_data_to_tokens(data, lang1, lang2, en_tokenizer, fr_tokenizer):
+    tokenized_data = []
+    for i in tqdm(range(len(data))):
+        tokenized_pair = (string_to_token_list(data[i][0], lang1, en_tokenizer, fr_tokenizer), string_to_token_list(data[i][1], lang2, en_tokenizer, fr_tokenizer))
+        tokenized_data.append(tokenized_pair)
+    return tokenized_data
+
+
+def data_preprocessing(en_tokenizer, fr_tokenizer, eng_str="en", fr_str="fr", data_pd=None, index_output=True):
     # initialize the language classes and get the data pairs (English, France)
     en_lang, fr_lang, data_pairs = read_lang(eng_str=eng_str, fr_str=fr_str, data_pd=data_pd)
     print("Got %s pairs" %len(data_pairs))
@@ -68,20 +81,37 @@ def data_preprocessing(eng_str="en", fr_str="fr", data_pd=None):
     print("Creating Dictionary...")
     # only save the lower case words
     for i in tqdm(range(len(data_pairs))):
-        en_lang.addSentence(data_pairs[i][0].lower())
-        fr_lang.addSentence(data_pairs[i][1].lower())
+        en_lang.addSentence(data_pairs[i][0].lower(), en_tokenizer, fr_tokenizer)
+        fr_lang.addSentence(data_pairs[i][1].lower(), en_tokenizer, fr_tokenizer)
     print("Counted words")
     print("en lenght:",en_lang.n_words)
     print("fr lenght:",fr_lang.n_words)
+    if index_output==True:
+        data_pairs = string_data_to_tokens(data_pairs, en_lang, fr_lang, en_tokenizer, fr_tokenizer)
     return en_lang, fr_lang, data_pairs
 
 
-en_lang, fr_lang, data_pairs = data_preprocessing(eng_str="en", fr_str="fr", data_pd=whole_data)
+def pickle_data(nrows=10000):
+    # tokenizers
+    en_tokenizer = get_tokenizer(tokenizer='spacy',language='en_core_web_sm')
+    fr_tokenizer = get_tokenizer(tokenizer='spacy',language='fr_core_news_sm')
+
+    # get the whole data from csv
+    whole_data = pd.read_csv("archive/en-fr.csv",encoding="utf-8", keep_default_na=False, nrows=nrows)
+
+    en_lang, fr_lang, data_pairs = data_preprocessing(en_tokenizer, fr_tokenizer, eng_str="en", fr_str="fr", data_pd=whole_data)
 
 
-with open('en_lang.pickle', 'wb') as handle:
-    pickle.dump(en_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open('fr_lang.pickle', 'wb') as handle:
-    pickle.dump(fr_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open('data_pairs.pickle', 'wb') as handle:
-    pickle.dump(data_pairs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('en_lang.pickle', 'wb') as handle:
+        pickle.dump(en_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('fr_lang.pickle', 'wb') as handle:
+        pickle.dump(fr_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('data_pairs.pickle', 'wb') as handle:
+        pickle.dump(data_pairs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def list_of_tokens_to_list_of_words(list_of_tokens, lang):
+    list_of_words = []
+    for token in list_of_tokens:
+        list_of_words.append(lang.index2word[token])
+    return list_of_words
+
---
+++ rnn.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pickle
+from torchtext.data.metrics import bleu_score
+import math
+import time
+import torch.optim as optim
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import numpy as np
+import data_loader
+
+class RNN:
+    def __init__(self, input_dim, hidden_dim, output_dim, target_lang, dropout_p=0.1):
+        super().__init__()
+        self.encoder = EncoderRNN(input_dim, hidden_dim, dropout_p=dropout_p)
+        self.decoder = DecoderRNN(hidden_dim, output_dim)
+        self.target_lang = target_lang
+
+    def train_epoch(self, data_pairs, encoder_optimizer, decoder_optimizer, criterion=bleu_score):
+        total_loss = 0
+        for pair in data_pairs:
+            input, target = pair
+            input = torch.tensor(input).unsqueeze(0)
+            target = torch.tensor(target).unsqueeze(0)
+
+            print(input.size())
+            print(target.size())
+            print("herererer")
+
+            encoder_optimizer.zero_grad()
+            decoder_optimizer.zero_grad()
+
+
+            encoder_outputs, encoder_hidden = self.encoder(input)
+            decoder_outputs, _, _ = self.decoder(encoder_outputs, encoder_hidden, target)
+
+            # This is where I'm struggling to get the BLEU Score as a tensor rather than a float
+            # I can only get BLEU to work with string inputs, but this and taking argmax in the
+            # next line aren't differentiable.
+            """maximizing_outputs = torch.topk(decoder_outputs, 1)[1].squeeze()
+            target_array = target.tolist()
+            maximizing_outputs_array = maximizing_outputs.tolist()
+            print(maximizing_outputs_array)
+            maximizing_outputs_strings = data_loader.list_of_tokens_to_list_of_words(maximizing_outputs_array, self.target_lang)
+            print(target_array)
+            target_strings = data_loader.list_of_tokens_to_list_of_words(target_array[0], self.target_lang)
+            print([maximizing_outputs_strings], [[target_strings]])
+            loss = criterion(
+                [maximizing_outputs_strings], [[target_strings]]
+            )"""
+
+            # If someone can come up with a nice BLEU function, hopefully this should work:
+            loss = criterion(
+                decoder_outputs, target
+            )
+
+
+            loss.backward()
+
+            encoder_optimizer.step()
+            decoder_optimizer.step()
+
+            total_loss += loss.item()
+
+        return total_loss / len(data_pairs)
+
+    def train(self, data_points, n_epochs, learning_rate=0.001, print_every=100, plot_every=100):
+        start = time.time()
+        plot_losses = []
+        print_loss_total = 0  # Reset every print_every
+        plot_loss_total = 0  # Reset every plot_every
+
+        encoder_optimizer = optim.Adam(self.encoder.parameters(), lr=learning_rate)
+        decoder_optimizer = optim.Adam(self.decoder.parameters(), lr=learning_rate)
+        criterion = bleu_score
+
+        for epoch in range(1, n_epochs + 1):
+            loss = self.train_epoch(data_points, encoder_optimizer, decoder_optimizer, criterion)
+            print_loss_total += loss
+            plot_loss_total += loss
+
+            if epoch % print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('%s (%d %d%%) %.4f' % (timeSince(start, epoch / n_epochs),
+                                             epoch, epoch / n_epochs * 100, print_loss_avg))
+
+            if epoch % plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
+
+        showPlot(plot_losses)
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, dropout_p=0.1):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+
+        self.embedding = nn.Embedding(input_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, batch_first=True)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, input):
+        embedded = self.dropout(self.embedding(input))
+        output, hidden = self.lstm(embedded)
+        return output, hidden
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_dim, output_dim, max_output_length=50, SOS_token=0):
+        super().__init__()
+        self.embedding = nn.Embedding(output_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, batch_first=True)
+        self.out = nn.Linear(hidden_dim, output_dim)
+        self.max_output_length = max_output_length
+        self.SOS_token = SOS_token
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long).fill_(self.SOS_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+
+        for i in range(self.max_output_length):
+            decoder_output, decoder_hidden = self.forward_step(decoder_input, decoder_hidden)
+            decoder_outputs.append(decoder_output)
+            print(decoder_output)
+
+            if (target_tensor is not None) and (i < target_tensor.size()[0]):
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
+
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        return decoder_outputs, decoder_hidden, None # We return `None` for consistency in the training loop
+
+    def forward_step(self, input, hidden):
+        output = self.embedding(input)
+        output = F.relu(output)
+        output, hidden = self.lstm(output, hidden)
+        output = self.out(output)
+        return output, hidden
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+#data_loader.pickle_data()
+
+print(bleu_score([["1", "2"]],[["1", "3"]]))
+
+
+print("about to do en_lang")
+with open('en_lang.pickle', 'rb') as handle:
+    en_lang = pickle.load(handle)
+print("done it")
+with open('fr_lang.pickle', 'rb') as handle:
+    fr_lang = pickle.load(handle)
+with open('data_pairs.pickle', 'rb') as handle:
+    data_pairs = pickle.load(handle)
+
+print(data_pairs[0])
+
+input_dim, hidden_dim, output_dim = en_lang.n_words, 100, fr_lang.n_words
+
+rnn = RNN(input_dim, hidden_dim, output_dim, fr_lang)
+
+rnn.train(data_pairs, 10)
+
+
+
+
