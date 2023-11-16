a975f4aea27f0744222f0569db5bb172f35cd3cd
Daniel <danieltvnguyen@gmail.com>
2023-11-16 12:58:31 -0500

    added abridged dataset

--- data_loader.py
+++ data_loader.py
@@ -5,8 +5,163 @@ import pandas as pd
 import pickle
 from tqdm import tqdm
 import string
+from pathlib import Path
+
+class EnFrDataset(Dataset):
+    def __init__(self, used_abridged_data:bool):
+        """_summary_
+
+        Args:
+            abridged (bool): Use the generated abridged dataset 
+
+        Returns:
+            _type_: _description_
+        """
+        self.use_abridged_data = used_abridged_data
+        self.full_dataset_path = Path("data/en-fr.csv")
+        self.abridged_dataset_path = Path("data/en-fr-abridged.csv")
+        
+        self.en_tokenizer = get_tokenizer(tokenizer='spacy',language='en_core_web_sm')
+        self.fr_tokenizer = get_tokenizer(tokenizer='spacy',language='fr_core_news_sm')
+        
+        self.process()
+        pass
+    
+    def process(self):
+         # Create abridged dataset if it doesnt exist and load either full or abridged data into self.ds 
+        # full_dataset_path = 'data/en-fr.csv'
+        # abridged_dataset_path = 'data/en-fr-abridged.csv'
+        self.full_dataset_path.parent.mkdir(parents=True, exist_ok=True) # make datafolder if it doesn't exist
+        
+        if not self.full_dataset_path.exists():
+            raise FileNotFoundError("The full dataset does not exist. Please download it from https://www.kaggle.com/datasets/dhruvildave/en-fr-translation-dataset/data and place in the /data folder.")
+        
+        if  self.use_abridged_data and not self.abridged_dataset_path.exists():      
+            print("Creating abridged dataset...")
+            full_dataset = pd.read_csv(self.full_dataset_path)
+            abridged_dataset = full_dataset.head(5000)
+            abridged_dataset.to_csv(self.abridged_dataset_path, index=False)
+        
+        self.ds = pd.read_csv(self.abridged_dataset_path, encoding="utf-8", keep_default_na=False) if self.use_abridged_data else pd.read_csv(self.full_dataset_path, encoding="utf-8", keep_default_na=False)
+        
+        self._data_preprocessing()
+        pass
+
+    def _data_preprocessing(self, eng_str="en", fr_str="fr"):
+        """_summary_
+
+        Args:
+            en_tokenizer (_type_): _description_
+            fr_tokenizer (_type_): _description_
+            eng_str (str, optional): _description_. Defaults to "en".
+            fr_str (str, optional): _description_. Defaults to "fr".
+            data_pd (_type_, optional): _description_. Defaults to None.
+            index_output (bool, optional): _description_. Defaults to True.
+
+        Returns:
+            _type_: _description_
+        """
+        # initialize the language classes and get the data pairs (English, France)
+        self.en_lang, self.fr_lang, self.data_pairs = self._read_lang(eng_str=eng_str, fr_str=fr_str, data_pd=self.ds) # Initialize language objects
+        for i in tqdm(range(len(self.data_pairs)), desc="Adding sentences to Langs amd geting data pairs..."): # create language dictionaries
+            self.en_lang.addSentence(self.data_pairs[i][0].lower(), self.en_tokenizer, self.fr_tokenizer)
+            self.fr_lang.addSentence(self.data_pairs[i][1].lower(), self.en_tokenizer, self.fr_tokenizer)
+
+        self.data_pairs = self._string_data_to_tokens(self.data_pairs, self.en_lang, self.fr_lang, self.en_tokenizer, self.fr_tokenizer) # converts sequence to tokens
+        #  return en_lang, fr_lang, data_pairs
+        pass
+            
+    def _read_lang(self, eng_str="en", fr_str="fr", data_pd=None):
+        # return if the dataformat is wrong
+        if type(self.ds) != pd.core.frame.DataFrame:
+            raise TypeError("Wrong dataframe format!!")
+            return
+        # get the pairs
+        pairs = [(self.ds[eng_str][i], self.ds[fr_str][i]) for i in tqdm(range(len(data_pd)))]
+            
+        # create the class objects of Langs for English and French to count the 
+        eng_lang = Langs("en")
+        fr_lang = Langs("fr")
+        return eng_lang, fr_lang, pairs
+    
+    def get_src_lang_size(self):
+        return self.en_lang.n_words
+    
+    def get_tgt_lang_size(self):
+        return self.fr_lang.n_words
+     
+     
+    def pickle_data(self, nrows=None):
+        en_lang, fr_lang, data_pairs = self._data_preprocessing(self.en_tokenizer, self.fr_tokenizer, eng_str="en", fr_str="fr")
+
+        abridge_tag = "_abridged" if self.use_abridged_data else ""
+
+        with open(f'data/en_lang{abridge_tag}.pickle', 'wb') as handle:
+            pickle.dump(en_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(f'data/fr_lang{abridge_tag}.pickle', 'wb') as handle:
+            pickle.dump(fr_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(f'data/data_pairs{abridge_tag}.pickle', 'wb') as handle:
+            pickle.dump(data_pairs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        pass
+
+    def list_of_tokens_to_list_of_words(list_of_tokens, lang):
+        list_of_words = []
+        for token in list_of_tokens:
+         list_of_words.append(lang.index2word[token])
+        return list_of_words
+
+    def _string_to_token_list(self, string, lang):
+        """Tokenize a single string in the given english/french language and return the list of tokens.
+
+        Args:
+            string (_type_): _description_
+            lang (_type_): _description_
+            en_tokenizer (_type_): _description_
+            fr_tokenizer (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        token_list = []
+        if lang.lang == "en":
+            words = self.en_tokenizer(string.lower())
+        else:
+            words = self.fr_tokenizer(string.lower())
+        for word in words:
+            token_list.append(lang.word2index[word])
+        return token_list
+ 
+    def _string_data_to_tokens(self, data, lang1, lang2, en_tokenizer, fr_tokenizer):
+        """Create tokenized pairs of english and french sentences
+
+        Args:
+            data (_type_): _description_
+            lang1 (_type_): _description_
+            lang2 (_type_): _description_
+            en_tokenizer (_type_): _description_
+            fr_tokenizer (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        tokenized_data = []
+                
+        for i in tqdm(range(len(data))):
+            tokenized_pair = (self._string_to_token_list(data[i][0], lang1), self._string_to_token_list(data[i][1], lang2))
+            tokenized_data.append(tokenized_pair)
+        return tokenized_data
 
 
+class CustomTokens(Enum):
+    """_summary_
+
+    Args:
+        _type_ (_type_): _description_
+    """
+    SOS = 0
+    EOS = 1
+    PAD = 2
+    pass
 
 # Langs class
 class Langs:
@@ -19,11 +174,15 @@ class Langs:
         self.word2count = {}
         # the dictionary to get the word through index (SOS: start of sentence, EOS: end of sentence)
         self.index2word = {0:"SOS", 1:"EOS"}
+        
         # put the punctuations inside the index2word dict
         for i in string.punctuation:
             self.index2word[len(self.index2word)] = i
         # the total number of special words
         self.n_words = len(self.index2word)
+        
+        
+        
     def addSentence(self, sentence, en_tokenizer, fr_tokenizer):
         if self.lang == "en":
             tokens = en_tokenizer(sentence)
@@ -40,78 +199,3 @@ class Langs:
             self.n_words += 1
         else:
             self.word2count[word] += 1
-
-def read_lang(eng_str="en", fr_str="fr", data_pd=None):
-    print("reading lines....")
-    # return if the dataformat is wrong
-    if type(data_pd) != pd.core.frame.DataFrame:
-        print("wrong data format!!")
-        return
-    # get the pairs
-    pairs = [(data_pd[eng_str][i], data_pd[fr_str][i]) for i in tqdm(range(len(data_pd)))]
-    
-    # create the class objects of Langs for English and French to count the 
-    eng_lang = Langs("en")
-    fr_lang = Langs("fr")
-    return eng_lang, fr_lang, pairs
-
-def string_to_token_list(string, lang, en_tokenizer, fr_tokenizer):
-    token_list = []
-    if lang.lang == "en":
-        words = en_tokenizer(string.lower())
-    else:
-        words = fr_tokenizer(string.lower())
-    for word in words:
-        token_list.append(lang.word2index[word])
-    return token_list
-
-def string_data_to_tokens(data, lang1, lang2, en_tokenizer, fr_tokenizer):
-    tokenized_data = []
-    for i in tqdm(range(len(data))):
-        tokenized_pair = (string_to_token_list(data[i][0], lang1, en_tokenizer, fr_tokenizer), string_to_token_list(data[i][1], lang2, en_tokenizer, fr_tokenizer))
-        tokenized_data.append(tokenized_pair)
-    return tokenized_data
-
-
-def data_preprocessing(en_tokenizer, fr_tokenizer, eng_str="en", fr_str="fr", data_pd=None, index_output=True):
-    # initialize the language classes and get the data pairs (English, France)
-    en_lang, fr_lang, data_pairs = read_lang(eng_str=eng_str, fr_str=fr_str, data_pd=data_pd)
-    print("Got %s pairs" %len(data_pairs))
-    # count the word occurences
-    print("Creating Dictionary...")
-    # only save the lower case words
-    for i in tqdm(range(len(data_pairs))):
-        en_lang.addSentence(data_pairs[i][0].lower(), en_tokenizer, fr_tokenizer)
-        fr_lang.addSentence(data_pairs[i][1].lower(), en_tokenizer, fr_tokenizer)
-    print("Counted words")
-    print("en lenght:",en_lang.n_words)
-    print("fr lenght:",fr_lang.n_words)
-    if index_output==True:
-        data_pairs = string_data_to_tokens(data_pairs, en_lang, fr_lang, en_tokenizer, fr_tokenizer)
-    return en_lang, fr_lang, data_pairs
-
-
-def pickle_data(nrows=None):
-    # tokenizers
-    en_tokenizer = get_tokenizer(tokenizer='spacy',language='en_core_web_sm')
-    fr_tokenizer = get_tokenizer(tokenizer='spacy',language='fr_core_news_sm')
-
-    # get the whole data from csv
-    whole_data = pd.read_csv("archive/en-fr.csv",encoding="utf-8", keep_default_na=False, nrows=nrows)
-
-    en_lang, fr_lang, data_pairs = data_preprocessing(en_tokenizer, fr_tokenizer, eng_str="en", fr_str="fr", data_pd=whole_data)
-
-
-    with open('en_lang.pickle', 'wb') as handle:
-        pickle.dump(en_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open('fr_lang.pickle', 'wb') as handle:
-        pickle.dump(fr_lang, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    with open('data_pairs.pickle', 'wb') as handle:
-        pickle.dump(data_pairs, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-def list_of_tokens_to_list_of_words(list_of_tokens, lang):
-    list_of_words = []
-    for token in list_of_tokens:
-        list_of_words.append(lang.index2word[token])
-    return list_of_words
-
---
+++ funnel_transformer.py
@@ -0,0 +1,193 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % num_heads == 0
+        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads # d_k dimensionality of the word embeddings in each head
+        
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_b = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+        
+    def scaled_dot_product_attention(self, Q, K, V, mask=None): 
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / math.sqrt(self.d_k) # Q^T*K and normalize by sqrt(d_k)
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9) # convert zeros to -1e9 to force softmax to zero
+        attention = nn.Softmax(dim=-1)(scores)
+        return torch.matmul(attention, V)
+    
+    def split_heads(self, x):
+        # x is tensor of all sequences and words
+        batch_size, seq_len, d_model = x.size()
+        return x.view(batch_size, seq_len, self.num_heads, self.d_k).transpose(1, 2)
+    
+    def combine_heads(self, x):
+        batch_size, _, seq_len, d_model = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, -1, self.num_heads * self.d_k)
+    
+    def forward(self, Q, K, V, mask=None):
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+        
+        attn_output = self.scaled_dot_product_attention(Q, K, V, mask)
+        output = self.W_o(self.combine_heads(attn_output))
+        return output
+
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+        
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        self.register_buffer('pe', pe.unsqueeze(0))
+        
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, mask):
+        attn_output = self.self_attn(x, x, x, mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x
+    
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(DecoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, enc_output, src_mask, tgt_mask):
+        attn_output = self.self_attn(x, x, x, tgt_mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        attn_output = self.cross_attn(x, enc_output, enc_output, src_mask)
+        x = self.norm2(x + self.dropout(attn_output))
+        ff_output = self.feed_forward(x)
+        x = self.norm3(x + self.dropout(ff_output))
+        return x
+    
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
+        super(Transformer, self).__init__()
+        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
+        self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+
+        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+
+        self.fc = nn.Linear(d_model, tgt_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def generate_mask(self, src, tgt):
+        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
+        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
+        seq_length = tgt.size(1)
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
+
+    def forward(self, src, tgt):
+        src_mask, tgt_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+        enc_output = src_embedded
+        for enc_layer in self.encoder_layers:
+            enc_output = enc_layer(enc_output, src_mask)
+
+        dec_output = tgt_embedded
+        for dec_layer in self.decoder_layers:
+            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
+
+        output = self.fc(dec_output)
+        return output
+# class FunnelTransformer(nn.Module):
+#     def __init__(self, num_tokens, emb_size, hidden_size, num_layers, num_heads, dropout):
+#         super().__init__()
+#         self.num_tokens = num_tokens
+#         self.emb_size = emb_size
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.num_heads = num_heads
+#         self.dropout = dropout
+        
+#         self.token_embedding = nn.Embedding(num_tokens, emb_size)
+#         self.positional_embedding = nn.Parameter(torch.zeros(1, num_tokens, emb_size))
+#         self.dropout_layer = nn.Dropout(dropout)
+        
+#         self.encoder_layers = nn.ModuleList([
+#             nn.TransformerEncoderLayer(d_model=emb_size, nhead=num_heads, dim_feedforward=hidden_size, dropout=dropout)
+#             for _ in range(num_layers)
+#         ])
+        
+#         self.decoder_layers = nn.ModuleList([
+#             nn.TransformerDecoderLayer(d_model=emb_size, nhead=num_heads, dim_feedforward=hidden_size, dropout=dropout)
+#             for _ in range(num_layers)
+#         ])
+        
+#         self.encoder_norm = nn.LayerNorm(emb_size)
+#         self.decoder_norm = nn.LayerNorm(emb_size)
+        
+#         self.output_layer = nn.Linear(emb_size, num_tokens)
+        
+#     def forward(self, input_tokens, target_tokens):
+#         input_embeddings = self.token_embedding(input_tokens) + self.positional_embedding
+#         target_embeddings = self.token_embedding(target_tokens) + self.positional_embedding
+        
+#         input_embeddings = self.dropout_layer(input_embeddings)
+#         target_embeddings = self.dropout_layer(target_embeddings)
+        
+#         for i in range(self.num_layers):
+#             input_embeddings = self.encoder_layers[i](input_embeddings)
+#             input_embeddings = self.encoder_norm(input_embeddings)
+            
+#             target_embeddings = self.decoder_layers[i](target_embeddings, input_embeddings)
+#             target_embeddings = self.decoder_norm(target_embeddings)
+        
+#         output_logits = self.output_layer(target_embeddings)
+#         return output_logits
