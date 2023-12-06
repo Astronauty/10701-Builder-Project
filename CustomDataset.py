55902beac98dc43ed992a4a7102fc6a97d8b8cc9
Joel Mire <jmire12@gmail.com>
2023-12-06 03:27:22 -0500

    minor bug fixes

--- CustomDataset.py
+++ CustomDataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 
@@ -6,9 +7,12 @@ class CustomDataset(Dataset):
 
     def __init__(self, tokenized_split_df_path):
         self.dataframe = pd.read_pickle(tokenized_split_df_path)
+        print(len(self.dataframe))
+        print(len(self.dataframe['en']))
+        print(len(self.dataframe['fr']))
 
     def __getitem__(self, idx):
-        return self.dataframe[idx]['en'], self.dataframe[idx]['fr']
+        return torch.tensor(self.dataframe.iloc[idx, self.dataframe.columns.get_loc("en")]), torch.tensor(self.dataframe.iloc[idx, self.dataframe.columns.get_loc("fr")])
 
     def __len__(self):
         return len(self.dataframe)
